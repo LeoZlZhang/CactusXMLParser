9208c00e0ff22439477763ca45fc103f77d72118
ezilizh <leo.b.zhang@ericsson.com>
2014-11-18 16:09:51 +0800

    Start

---
+++ src/Search.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ezilizh
+ * Date: 11/18/14
+ * Time: 4:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Search
+{
+}
